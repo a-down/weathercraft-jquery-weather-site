d95230dc25e1c58047e2040c957357a9ea7ee346
a-down <136649391+a2downing@users.noreply.github.com>
2023-09-27 20:19:11 -0500

    reader full today card

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,55 +1,21 @@
+const apiKey = process.env.API_KEY
 const todayGrid = $('.today-grid')
 
-const testData = [
-  {
-    temperature: '50',
-    time: '8 am'
-  },
-  {
-    temperature: '67',
-    time: '10 am'
-  },
-  {
-    temperature: '70',
-    time: '12 pm'
-  },
-  {
-    temperature: '74',
-    time: '2 pm'
-  },
-  {
-    temperature: '80',
-    time: '4 pm'
-  },
-  {
-    temperature: '84',
-    time: '6 pm'
-  },
-  {
-    temperature: '78',
-    time: '8pm'
-  },
-  {
-    temperature: '67',
-    time: '10 pm'
-  },
-  {
-    temperature: '62',
-    time: '12 pm'
-  },
-]
 
-function displayTodayWeather() {
-  let i = 0
+// append information inside today card
+function fillTodayCard(data) {
+  console.log(data)
   let barColor
-  testData.map((data) => {
-    i / 2 === 0 ? barColor = '' : barColor = 'alt-element'
+
+  for (i = 0; i < 9; i++) {
+    console.log(i)
+    i % 2 !== 0 ? barColor = '' : barColor = 'alt-element'
     todayGrid.append(
       `<div class="temp-graph-element">
       <div>
-        <small>${data.temperature}°</small>
-        <div class="temp-graph-bar ${barColor}" style="height: ${(data.temperature + 20) / 140}px"></div>
-        <small class="bold-small">${data.time}</small>
+        <small>${data[i].temperature}°</small>
+        <div class="temp-graph-bar ${barColor}" style="height: ${(data[i].temperature + 20) / 140}px"></div>
+        <small class="bold-small">${data[i].time}</small>
       </div>
     
       <div class="flex-col">
@@ -63,11 +29,15 @@ function displayTodayWeather() {
       </div>
     </div>`
     )
-    i + 1
-  })
+  }
 }
 
-displayTodayWeather()
+async function fillCards() {
+  const res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${apiKey}`)
+  const data = await res.json()
+  fillTodayCard(data)
+}
+fillCards()
 
 
 
