85ce72cd9d0b9e012418afa47c754518f1099488
a-down <136649391+a2downing@users.noreply.github.com>
2023-09-27 21:13:17 -0500

    dynamically fill all quick cards

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,61 @@
-const apiKey = process.env.API_KEY
+const nowCard = $('.now-card')
+const tonightCard = $('.tonight-card')
 const todayGrid = $('.today-grid')
 
+// append information inside now card
+function fillNowCard(data) {
+  console.log(data)
+  nowCard.append(
+    `<div class="flex-between">
+    <p>${data.temp}</p>
+    <p class="gray-text">Temp</p>
+  </div>
+
+  <div class="flex-between">
+    <p>${data.feelsLike}</p>
+    <p class="gray-text">Feels Like</p>
+  </div>
+
+  <div class="flex-around">
+    <div class="quick-weather-card-visual">
+      <img src="https://placehold.co/48" />
+      <small>${data.precip}</small>
+    </div>
+
+    <div class="quick-weather-card-visual">
+      <img src="https://placehold.co/48" />
+      <small>${data.wind}</small>
+    </div>
+  </div>`
+  )
+}
+
+function fillTonightCard(data) {
+  console.log(data)
+  tonightCard.append(
+    `<div class="flex-between">
+    <p>${data.temp}</p>
+    <p class="gray-text">Temp</p>
+  </div>
+
+  <div class="flex-between">
+    <p>${data.feelsLike}</p>
+    <p class="gray-text">Feels Like</p>
+  </div>
+
+  <div class="flex-around">
+    <div class="quick-weather-card-visual">
+      <img src="https://placehold.co/48" />
+      <small>${data.precip}</small>
+    </div>
+
+    <div class="quick-weather-card-visual">
+      <img src="https://placehold.co/48" />
+      <small>${data.wind}</small>
+    </div>
+  </div>`
+  )
+}
 
 // append information inside today card
 function fillTodayCard(data) {
@@ -8,7 +63,6 @@ function fillTodayCard(data) {
   let barColor
 
   for (i = 0; i < 9; i++) {
-    console.log(i)
     i % 2 !== 0 ? barColor = '' : barColor = 'alt-element'
     todayGrid.append(
       `<div class="temp-graph-element">
@@ -32,10 +86,21 @@ function fillTodayCard(data) {
   }
 }
 
-async function fillCards() {
-  const res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${apiKey}`)
+async function getApiKey() {
+  const res = await fetch(`../public/assets/apiKey.json`)
   const data = await res.json()
-  fillTodayCard(data)
+  return data.API_KEY
+}
+
+async function fillCards() {
+  const apiKey = await getApiKey()
+  const res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${apiKey}`)
+  // const res = await fetch('../public/assets/testData.json')
+  const data = await res.json()
+  console.log(data)
+  fillTodayCard(data[0])
+  fillNowCard(data[1][0])
+  fillTonightCard(data[1][0])
 }
 fillCards()
 
