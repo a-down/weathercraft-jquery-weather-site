9a8292dd3dcfc7bc90cdf2b211fca2d8331c51db
a-down <136649391+a2downing@users.noreply.github.com>
2023-09-28 21:22:08 -0500

    production process

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../", 'client/build')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve( __dirname, "../", 'client/build/index.html'))
+  })
+}
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html'))
