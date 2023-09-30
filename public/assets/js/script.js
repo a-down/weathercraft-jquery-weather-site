c2534fb10ae30c70b541c81fdbccfb0db59d578f
a-down <136649391+a2downing@users.noreply.github.com>
2023-09-30 18:25:32 -0500

    favorite button styling

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -194,7 +194,7 @@ async function displayQuickWeather(data) {
     $('.favorite-wrapper').append(`<img src="./assets/icons/star.svg" class="icon-link"/>`)
 
   } else {
-    $('.favorite-wrapper').append(`<button class="button">Set as Favorite</button>`)
+    $('.favorite-wrapper').append(`<button class="button favorite-button">Set as Favorite</button>`)
   }
     
 }
